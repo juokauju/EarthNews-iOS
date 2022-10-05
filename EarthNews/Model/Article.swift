ace46823fd302eb534363859c49b14d03618693e
Justina Siaulyte <103490410+juokauju@users.noreply.github.com>
2022-10-05 12:44:34 +0300

    add Article model, MVVM

---
+++ EarthNews/Model/Article.swift
@@ -0,0 +1,42 @@
+//
+//  Article.swift
+//  EarthNews
+//
+//  Created by jimi bird on 2022-10-05.
+//
+
+
+import UIKit
+
+struct Article {
+    let title: String
+    let description: String
+    let content: String
+    let url: URL
+    let imageUrl: URL
+    let date: Date
+    let source: Source
+    
+    var articleImage: UIImage?
+}
+
+extension Article: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case title, description, content, url, source
+        case imageUrl = "image"
+        case date = "publishedAt"
+        
+    }
+}
+
+struct Source {
+    let name: String
+    let url: URL
+}
+
+extension Source: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case name, url
+    }
+}
+
---
+++ EarthNews/ViewModels/ArticlesViewModelProtocol.swift
@@ -0,0 +1,23 @@
+//
+//  ArticlesViewModel.swift
+//  EarthNews
+//
+//  Created by Justina Siaulyte on 2022-09-30.
+//
+
+import Foundation
+
+protocol ArticlesViewModel {
+    var articles: [Article] { get }
+    
+    func fetchArticles()
+}
+
+extension ArticlesViewModel {
+    var actionOnDatabaseIcon: String {
+        get { "circle" }
+        set { }
+    }
+    
+    func actOnDatabase() { }
+}
