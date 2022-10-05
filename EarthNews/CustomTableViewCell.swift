706e9cd58fa83066806e6543f10356b9f69728f1
Justina Siaulyte <justina.siautyte@protonmail.com>
2022-10-05 11:47:33 +0300

    Create Table View Controller, Custom Cell, Article View Model protocol.

---
+++ EarthNews/CustomTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  CustomTableViewCell.swift
+//  EarthNews
+//
+//  Created by jimi bird on 2022-09-30.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    static let identifier = "CustomTableViewCell"
+    
+    static var nib: UINib {
+        UINib(nibName: "CustomTableViewCell", bundle: nil)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    @IBOutlet var date: UILabel!
+    @IBOutlet var title: UILabel!
+    @IBOutlet var subtitle: UILabel!
+    @IBOutlet var source: UILabel!
+    
+    @IBOutlet var cellImage: UIImageView!
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
